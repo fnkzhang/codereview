7ee3ac561ea5a5ee46c969a4956bec05d32df39f
luke Brenner <luke.t.brenner@gmail.com>
2024-01-31 12:18:33 -0800

    Added basic monaco editor.

--- react/src/App.js
+++ react/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the monaco editor.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Editor 
+          height="90vh"
+          defaultLanguage="python"
+          defaultValue="print('Hello, world')" 
+        />;
       </header>
     </div>
   );
