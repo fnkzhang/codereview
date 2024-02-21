5946e574b55ba53b284ffc288edc84a10309a2b3
hain05 <122949885+hain05@users.noreply.github.com>
2024-02-20 22:32:43 -0800

    revised comment functions and added new ones

--- react/src/api/APIUtils.js
+++ react/src/api/APIUtils.js
@@ -81,3 +81,84 @@ export async function getDiff(proj_id, doc_id, diff_id) {
     .concat(diff_id).concat('/get'), headers)
     .then(response => response.json())
 }
+
+export async function createComment(diff_id, author_id, reply_to_id, content) {
+  let oAuthToken = "fake oauth token"
+  let headers = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Authorization": `Bearer ${oAuthToken}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "diff_id": diff_id,
+      "author_id": author_id,
+      "reply_to_id": reply_to_id,
+      "content": content
+    })
+  };
+
+  return await fetch(`/api/diffs/${diff_id}/comment/create`, headers)
+    .then(response => response.json())
+}
+
+export async function getCommentsOnDiff(diff_id) {
+  let oAuthToken = "fake oauth token"
+  let headers = {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Authorization": `Bearer ${oAuthToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+
+  return await fetch(`/api/diffs/${diff_id}/comments/get`, headers)
+    .then(response => response.json())
+}
+
+export async function getSubcommentsOnComment(comment_id) {
+  let oAuthToken = "fake oauth token"
+  let headers = {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Authorization": `Bearer ${oAuthToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+
+  return await fetch(`/api/comments/${comment_id}/subcomments/get`, headers)
+    .then(response => response.json())
+}
+
+export async function editComment(comment_id) {
+  let oAuthToken = "fake oauth token"
+  let headers = {
+    method: "PUT",
+    mode: "cors",
+    headers: {
+      "Authorization": `Bearer ${oAuthToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+
+  return await fetch(`/api/comments/${comment_id}/edit`, headers)
+    .then(response => response.json())
+}
+
+export async function deleteComment(comment_id) {
+  let oAuthToken = "fake oauth token"
+  let headers = {
+    method: "DELETE",
+    mode: "cors",
+    headers: {
+      "Authorization": `Bearer ${oAuthToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+
+  return await fetch(`/api/comments/${comment_id}/delete`, headers)
+    .then(response => response.json())
+}
