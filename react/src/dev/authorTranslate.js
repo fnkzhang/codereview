a33cf85aa7e3306228b4338ae70f64f46cca1b75
hain05 <hcnguyen@ucdavis.edu>
2024-02-24 03:49:03 -0800

    added extra names to authorTranslate

--- react/src/dev/authorTranslate.js
+++ react/src/dev/authorTranslate.js
@@ -1,7 +1,9 @@
 export function mapToName (num) {
     const names = {
         1: "ltbrenner@ucdavis.edu",
-        2: "sichuan@ucdavis.edu"
+        2: "sichuan@ucdavis.edu",
+        3: "hcnguyen@ucdavis.edu",
+        4: "fnkzhang@ucdavis.edu"
     }
 
     return names[num]
