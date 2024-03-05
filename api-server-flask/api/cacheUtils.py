1f4af695168c013c57f10f7a0963ac4f106ab052
Hai Nguyen <hcnguyen@ucdavis.edu>
2024-03-05 02:38:53 -0800

    file system caching, clear cache when document gets updated w/o requesting blob

--- api-server-flask/api/cacheUtils.py
+++ api-server-flask/api/cacheUtils.py
@@ -1,15 +1,64 @@
 from flask_caching import Cache
+from google.cloud import pubsub_v1
+
+subscriber = pubsub_v1.SubscriberClient()
+publisher = pubsub_v1.PublisherClient()
 
 cacheConfig = {
-    "CACHE_TYPE": "SimpleCache"
+    "CACHE_TYPE": "FileSystemCache",
+    "CACHE_DIR": "./../cr_cache",
+    "CACHE_THRESHOLD": 500
 }
 
+"""
+Caching
+"""
 def initCache(app):
     global cache
     cache = Cache(app, config=cacheConfig)
+
+    subscribeToTopics()
+    return
 
 def getValueFromCache(key):
     return cache.get(key)
 
 def addToCacheWithTimeout(key, value, timeout):
     cache.set(key, value, timeout)
+    return
+
+def deleteValueFromCache(key):
+    cache.delete(key)
+    return
+
+"""
+Pub/Sub
+"""
+def subscribeToTopics():
+    subscribeToDocumentUpdate()
+    return
+
+"""
+Pub/Sub for Document Updates
+"""
+def subscribeToDocumentUpdate():
+    def handleDocumentUpdate(message):
+        doc_id = message.data.decode()
+        cache.delete(doc_id)
+
+        message.ack()
+        return
+
+    subscriptionPath = subscriber \
+        .subscription_path("codereview-413200", "document-updates-sub")
+    
+    subscriber.subscribe(subscriptionPath, handleDocumentUpdate)
+    return
+
+def publishDocumentUpdate(doc_id):
+    topicPath = publisher \
+        .topic_path("codereview-413200", "document-updates")
+
+    message = doc_id.encode()
+    publisher.publish(topicPath, message)
+    return
