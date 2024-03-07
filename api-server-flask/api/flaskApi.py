5633183e3ac2da9150932e7eae6acdb3e8f7d521
Hai Nguyen <hcnguyen@ucdavis.edu>
2024-03-06 17:16:54 -0800

    moved stuff I thought was useless/test to another file

--- api-server-flask/api/flaskApi.py
+++ api-server-flask/api/flaskApi.py
@@ -1,123 +1,32 @@
-from cloudSql import connectCloudSql
 from flask import Flask, request, jsonify
+app = Flask(__name__)
+
 from flask_cors import CORS
-from cacheUtils import initCaches, publishTopicUpdate, documentCache, diffCache, commentsCache
+CORS(app)
+
+try:
+    from testRoutes import createTable, dropUserProjectRelationTable, \
+        testInsert, grabData, sendData
+except:
+    pass
+
+from cloudSql import connectCloudSql
+from cacheUtils import publishTopicUpdate, documentCache, diffCache, commentsCache
 from utils import *
 from diff_match_patch import diff_match_patch
 from google.oauth2 import id_token
 from google.auth.transport import requests
-from sqlalchemy import Table, Column, String, Integer, MetaData, insert, select, update, delete
+from sqlalchemy import insert, update, delete
 from sqlalchemy.orm import sessionmaker
 import models
-from cloudSql import connectCloudSql
 
 from utils import engine
 
 #Todo hide later
 CLIENT_ID = "474055387624-orr54rn978klbpdpi967r92cssourj08.apps.googleusercontent.com"
 
-app = Flask(__name__)
-CORS(app)
-initCaches(app)
-print(documentCache, diffCache, commentsCache)
-
 engine = connectCloudSql()
 Session = sessionmaker(engine)
-
-
-
-# Remove Later
-class User():
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    email = Column(String(50))
-
-
-metaData = MetaData()
-table = Table('testTable', metaData,
-            Column('id', Integer(), primary_key=True),
-            Column('name', String(50), nullable=False),
-            Column('email', String(50), nullable=False),
-            )
-
-# Remove Later for testing
-@app.route('/createTable')
-def createTable():
-    engine = connectCloudSql()
-    
-    models.Comment.metadata = models.Base.metadata
-
-    models.Comment.metadata.create_all(engine)
-    models.User.metadata = models.Base.metadata
-    models.User.metadata.create_all(engine)
-    models.UserProjectRelation.metadata = models.Base.metadata
-    models.UserProjectRelation.metadata.create_all(engine)
-    #metaData.create_all(engine)
-    print("Table was created")
-    return "Created Table"
-
-@app.route('/dropUserProjectRelationTable')
-def dropUserProjectRelationTable():
-    models.UserProjectRelation.__table__.drop(engine)
-    return True
-
-@app.route('/insert')
-def testInsert():
-    #engine = connectCloudSql()
-    with engine.connect() as conn:
-        stmt = insert(table).values(name="PungeBob", email="testEmail@gmail.com")
-
-        conn.execute(stmt)
-        conn.commit()
-
-    return "tested"
-
-@app.route('/testGrabData')
-def grabData():
-    #engine = connectCloudSql()
-
-    with engine.connect() as conn:
-        stmt = select(table).where(table.c.email == "testEmail@gmail.com")
-
-        result = conn.execute(stmt)
-        result = result.mappings().all()
-
-        retArray = []
-        # Recreate Dict from SQLAlchemy Row and return
-        # Can't Find any alternatives that worked rn maybe in future
-        for row in result:
-            d = {}
-            d["id"] = row.id
-            d["name"] = row.name
-            d["email"] = row.email
-
-            retArray.append(d)
-
-    returnArray = {
-        "success": True,
-        "reason": "",
-        "body": retArray,
-    }
-    
-    return returnArray
-# End Remove later
-
-# Takes in json with "code" section
-@app.route('/api/sendData', methods=["POST"])
-def sendData():
-    inputBody = request.get_json()
-
-    # Check valid request json
-    if "credential" not in inputBody or "code" not in inputBody:
-        return { "success": False,
-                "reason": "Invalid JSON Provided",
-                "body": {}
-        }
-    
-    return { "success": True,
-            "reason": "N/A",
-            "body": inputBody
-            }
 
 @app.route('/api/user/authenticate', methods=["POST"])
 def authenticator():
