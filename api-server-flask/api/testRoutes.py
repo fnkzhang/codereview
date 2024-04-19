f3b8931c866ef260a27a271a1617303e11562292
Hai Nguyen <hcnguyen@ucdavis.edu>
2024-04-18 23:38:45 -0700

    llm test route

--- api-server-flask/api/testRoutes.py
+++ api-server-flask/api/testRoutes.py
@@ -1,4 +1,23 @@
 from flaskApi import app
 
+from flask import request
+
 # PUT TEST/TEMP STUFF HERE
 # @app.route
+
+from llm import get_chat_response
+# curl -X POST http://127.0.0.1:5000/api/llm/test -H 'Content-Type: application/json' -d '{"input": "hi"}'
+@app.route("/api/llm/test", methods=["POST"])
+def test_llm():
+    data = request.get_json()
+    input = data.get("input")
+
+    response = get_chat_response(input)
+
+    if response is None:
+        return {
+            "success": False,
+            "reason": "LLM Error"
+        }
+
+    return response
