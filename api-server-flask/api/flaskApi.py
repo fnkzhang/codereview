b0b2ea253c1a2757188b9bef2937151a5df048be
fnkzhang <87836307+fnkzhang@users.noreply.github.com>
2024-05-09 16:54:28 -0700

    Update flaskApi.py

--- api-server-flask/api/flaskApi.py
+++ api-server-flask/api/flaskApi.py
@@ -1455,6 +1455,8 @@ def pullToExistingProject(proj_id):
                 else:
                     doc_id = createNewDocument(file_content.name, pathToFolderID[path], proj_id, file_content.decoded_content)
                     updated_files.append(doc_id)
+            except:
+                pass
         print(pathToFolderID)
     for doc_to_delete in docs_to_delete:
         deleteDocumentUtil(doc_to_delete)
