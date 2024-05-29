0973b67c3c067e4f342a7c09a7fd0e33fea461c6
Hai Nguyen <hcnguyen@ucdavis.edu>
2024-05-29 14:39:50 -0700

    created blank unit tests for all routes

--- api-server-flask/api/new_ok_test.py
+++ api-server-flask/api/new_ok_test.py
@@ -39,22 +39,24 @@ def client(app):
 Unit Tests for authRoutes.py
 """
 
-def test_authenticator_invalid_request(client):
+def test_authenticator(client):
     
+    # Invalid Requests
     response = client.post("/api/user/authenticate")
     assert response.json["success"] == False
 
-def test_authenticator_valid_request(client):
-    
+    # Valid Requests
     response = client.post("/api/user/authenticate", headers={"Authorization": "oAuthToken"})
     print(response.json["reason"])
     assert response.json["success"] == True
 
-def test_signUp_invalid_request(client):
+def test_signUp(client):
+
+    # Invalid requests
     response = client.post("/api/user/signup")
     assert response.json["success"] == False
-    
-def test_signUp_valid_request(client):
+
+    # Valid Requests
     with patch('authRoutes.createNewUser', autospec=True) as mock_createNewUser:
 
         mock_createNewUser.return_value = True
@@ -71,7 +73,9 @@ def test_signUp_valid_request(client):
             response = client.post("/api/user/signup", headers={"Authorization": "oAuthToken"})
             assert response.json["success"] == True
 
-def test_checkIsValidUser_invalid_request(client):
+def test_checkIsValidUser(client):
+
+    # Invalid Requests
     with patch('authRoutes.userExists', autospec=True) as mock_UserExists:
 
         mock_UserExists.return_value = False
@@ -82,7 +86,7 @@ def test_checkIsValidUser_invalid_request(client):
         response = client.post("/api/user/isValidUser", headers={"Email": "fake-email@fake-domain.com"})
         assert response.json["success"] == False
 
-def test_checkIsValidUser_valid_request(client):
+    # Valid Requests
     with patch('authRoutes.userExists', autospec=True) as mock_UserExists:
 
         mock_UserExists.return_value = False
@@ -96,3 +100,510 @@ def test_checkIsValidUser_valid_request(client):
 """
 Unit Tests for commentRoutes.py
 """
+
+def test_createComment(client):
+
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_resolveComment(client):
+
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_editComment(client):
+
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_deleteComment(client):
+
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+"""
+Unit Tests for commitRoutes.py
+"""
+
+def test_createCommit(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_checkIfNewerCommitExists(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getCommitDifferences(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getCommitDiffCareAboutLast(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_bulkAddToCommit(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_bulkDeleteFromCommit(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_commitCommit(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_setReviewedCommit(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_closeCommit(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_approveCommit(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_deleteWorkingCommit(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getCommitFolderTree(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getUserWorkingCommitForProject(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getAllLatestCommitComments(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+"""
+Unit Tests for documentRoutes.py
+"""
+
+def test_getDocument(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_createDocument(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_deleteDocument(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_renameDocument(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_moveDocument(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getAllDocumentCommittedSnapshots(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getAllDocumentCommittedSnapshotsIncludingWorking(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_changeDocumentSnapshot(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getAllCommentsForDocument(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+"""
+Unit Tests for folderRoutes.py
+"""
+
+def test_getFolder(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_createFolder(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_deleteFolder(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_renameFolder(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_moveFolder(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+"""
+Unit Tests for githubRoutesRoutes.py
+"""
+
+def test_getUserGithubStatus(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_addGithubToken(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getGithubRepositoryBranches(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_pullToNewProject(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_pullToExistingProject(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_pushToNewBranch(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_pushToExistingBranch(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+
+
+"""
+Unit Tests for llmRoutes.py
+"""
+
+def test_implement_code_changes_from_comment(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_suggest_comment_from_code(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+
+"""
+Unit Tests for projectRoutes.py
+"""
+
+def test_getProject(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_createProject(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_deleteProject(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_renameProject(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getProjectCommittedCommits(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getProjectLatestCommit(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getProjectDocuments(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+"""
+Unit Tests for snapshotRoutes.py
+"""
+
+def test_getSnapshot(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_createSnapshot(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_deleteSnapshot(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getCommentsOnSnapshot(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+"""
+Unit Tests for userAndPermissionsRoutes.py
+"""
+
+def test_getAllUserProjects(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_addUser(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_transferProjectOwnership(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_importPermissions(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_addUserAdmin(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_removeUser(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
+
+def test_getUsersWithAccessToProject(client):
+    
+    # Invalid Requests
+
+    # Valid Requests
+
+    pass
